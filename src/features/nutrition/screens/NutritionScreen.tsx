624b40c3d9e9f15d903378d41281aa247be8dac2
KarolinaGajos <142922712+KarolinaGajos@users.noreply.github.com>
2025-10-10 15:08:16 +0200

    feat: implement NutritionScreen and update FoodIndex to use it

---
+++ src/features/nutrition/screens/NutritionScreen.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "expo-router";
+import { View } from "react-native";
+
+export const NutritionScreen = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+    </View>
+  );
+};
+export default NutritionScreen;
