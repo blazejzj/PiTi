9353dc2411ad671fdda80ea947be3763d70e6cf2
Stefan Strand <stefanboytchev@hotmail.com>
2025-10-11 21:40:12 +0200

    feat(profle): added stats bar with horizontal line separators for visual claroty.

--- src/features/profile/screens/ProfileScreen.tsx
+++ src/features/profile/screens/ProfileScreen.tsx
@@ -21,18 +21,31 @@ export default function ProfileScreen() {
             <ScrollView>
                 {/* ScrollView vs FlatList? Profile page is relatively short, not THAT many elements to be rendered. Need for lazy render with Flatlist? Je ne sais pas. */}
                 <View className="">
-                    <View className="items-center bg-amber-200 rounded-2xl p-5 mt-2">
+                    <View className="items-center bg-amber-200 rounded-2xl py-3 mt-2">
                         <View className="w-24 h-24 rounded-full bg-red-500 mb-3" />
                         <Text className="text-lg font-semibold color-white">
                             {userName}
                         </Text>
 
-                        <View className="flex-row justify between w-full mt-5">
-                            <View className="flex-1 items-center">
+                        <View className="h-px w-full bg-neutral-950 mt-2" />
+                        {/* Stats row */}
+                        <View className="flex-row justify-between w-full py-3 px-5">
+                            <View className="flex-1 items-center mt-2">
+                                <Text className="font-bold">32</Text>
+                                <Text className="text-neutral-600">Økter</Text>
+                            </View>
+                            <View className="flex-1 items-center mt-2">
                                 <Text className="font-bold">100 kg</Text>
                                 <Text className="text-neutral-600">Vekt</Text>
                             </View>
+                            <View className="flex-1 items-center mt-2">
+                                <Text className="font-bold">2750</Text>
+                                <Text className="text-neutral-600">
+                                    Kalorimål
+                                </Text>
+                            </View>
                         </View>
+                        <View className="h-px w-full bg-neutral-950 mt-2" />
                     </View>
                 </View>
             </ScrollView>
