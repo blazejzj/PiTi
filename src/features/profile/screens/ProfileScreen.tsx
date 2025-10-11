2abe5c607fc1c28163dae0c1566021db29d03623
Stefan Strand <stefanboytchev@hotmail.com>
2025-10-11 14:48:52 +0200

    feat(profiel): Top bar and avatar pic plus username added. Top part simple finished

--- src/features/profile/screens/ProfileScreen.tsx
+++ src/features/profile/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import { router } from "expo-router";
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, ScrollView } from "react-native";
 
 export default function ProfileScreen() {
     const userName = "Ola Nordamnn";
@@ -17,6 +17,18 @@ export default function ProfileScreen() {
                     <Text className="text-2xl">hmbgBtn</Text>
                 </Pressable>
             </View>
+
+            <ScrollView>
+                {/* ScrollView vs FlatList? Profile page is relatively short, not THAT many elements to be rendered. Need for lazy render with Flatlist? Je ne sais pas. */}
+                <View className="">
+                    <View className="items-center bg-amber-200 rounded-2xl p-5 mt-2">
+                        <View className="w-24 h-24 rounded-full bg-red-500 mb-3" />
+                        <Text className="text-lg font-semibold color-white">
+                            {userName}
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
