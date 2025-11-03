39bf5e781a08d6f72225f9c07195af009aa2ff87
Stefan Strand <stefanboytchev@hotmail.com>
2025-11-03 22:53:19 +0100

    added first part of test for createprofilescreen - testing happy paht, all works goood. passes.

---
+++ src/__tests__/profile/CreateProfileScreen.test.tsx
@@ -0,0 +1,73 @@
+import {
+    render,
+    screen,
+    fireEvent,
+    waitFor,
+} from "@testing-library/react-native";
+
+// mock router for nav...
+const mockRouter = { replace: jest.fn(), push: jest.fn(), back: jest.fn() };
+jest.mock("expo-router", () => ({
+    useRouter: () => mockRouter,
+}));
+
+//mock useProfile hook..*
+
+jest.mock("../../features/profile/hooks/useProfile", () => ({
+    useProfile: jest.fn(),
+}));
+
+jest.mock("../../features/profile/api/profileRepo", () => ({
+    upsertUserProfile: jest.fn(),
+}));
+
+jest.mock("react-native-toast-message", () => ({
+    __esModule: true,
+    default: { show: jest.fn() },
+}));
+
+import { useProfile } from "../../features/profile/hooks/useProfile";
+import { upsertUserProfile } from "../../features/profile/api/profileRepo";
+import Toast from "react-native-toast-message";
+import CreateProfileScreen from "../../features/profile/screens/CreateProfileScreen";
+
+describe("CreateProfileScreen", () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+        (useProfile as jest.Mock).mockReturnValue({
+            userId: "user-666",
+        });
+    });
+
+    it("saves profile successfully on valid submission and navs to profiule screen", async () => {
+        (upsertUserProfile as jest.Mock).mockResolvedValue({});
+        render(<CreateProfileScreen />);
+
+        fireEvent.changeText(screen.getByPlaceholderText("Age"), "30");
+        fireEvent.changeText(
+            screen.getByPlaceholderText("male / female / other"),
+            "male"
+        );
+        fireEvent.changeText(screen.getByPlaceholderText("Height (cm)"), "180");
+        fireEvent.changeText(screen.getByPlaceholderText("Weight (kg)"), "75");
+        fireEvent.changeText(screen.getByPlaceholderText("kcal"), "2500");
+        fireEvent.changeText(screen.getByPlaceholderText("Carbs (g)"), "300");
+        fireEvent.changeText(screen.getByPlaceholderText("Fat (g)"), "70");
+        fireEvent.changeText(screen.getByPlaceholderText("Protein (g)"), "150");
+
+        fireEvent.press(screen.getByText("Save profile"));
+
+        await waitFor(() => {
+            expect(upsertUserProfile).toHaveBeenCalledWith(
+                "user-666",
+                expect.any(Object)
+            );
+            expect(Toast.show).toHaveBeenCalledWith(
+                expect.objectContaining({
+                    type: "success",
+                })
+            );
+            expect(mockRouter.replace).toHaveBeenCalledWith("/(home)/profile");
+        });
+    });
+});
